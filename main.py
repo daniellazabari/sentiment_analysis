d522432d82b55b06f204a206d240251c58bee791
daniella.zabari <daniella.zabari@post.idc.ac.il>
2022-09-08 00:28:47 +0300

    Created a normalization function

--- main.py
+++ main.py
@@ -1,33 +1,27 @@
 import nltk
-
 # nltk.download('twitter_samples')
 # nltk.download('punkt')
 # nltk.download('wordnet')
 # nltk.download('averaged_perceptron_tagger')
 # nltk.download('omw-1.4')
+# nltk.download('stopwords')
 from nltk.tag import pos_tag
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.corpus import twitter_samples
+from nltk.corpus import stopwords
+import re, string
 
-# Tokenize the data
-positive_tweets = twitter_samples.strings('positive_tweets.json') # 5000 tweets with positive sentiments
-negative_tweets = twitter_samples.strings('negative_tweets.json') # 5000 tweets with negative sentiments
-text = twitter_samples.strings('tweets.20150430-223406.json') # 20000 tweets with no sentiments
-tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-
-# Normalize the data using lemmatization, which normalized a word with the context of vocabulary and
-# morphological analysis of words in text.
-# Before running a lemmatizer, you need to determine the context for each word in the text using a 
-# tagging algorithm.
-def lemmatize_sentence(tokens):
-    '''
-    In this function, we first generate the tags for each token in the text, and 
-    then lemmatize each word using the tag. 
-    '''
-    lemmatizer = WordNetLemmatizer()
-    lemmatized_sentence = []
+# Removing noise from the data & normalizing the data
+def remove_noise(tweet_tokens, stop_words=()):
+    cleaned_tokens = []
     # Get the position tag of each token of a tweet
-    for word, tag in pos_tag(tokens):
+    for token, tag in pos_tag(tweet_tokens):
+        # Remove noise from the data
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'\
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+','', token) # Remove hyperlinkes 
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token) # Remove twittwer handles in replies
+        
+        # Normalize the data using lemmatization
         # If the tag starts with NN, the token is assigned as a noun
         if tag.startswith('NN'):
             pos = 'n'
@@ -36,5 +30,35 @@ def lemmatize_sentence(tokens):
             pos = 'v'
         else:
             pos = 'a'
-        lemmatized_sentence.append(lemmatizer.lemmatize(word, pos))
-    return lemmatized_sentence
+        
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            cleaned_tokens.append(token.lower())
+    
+    return cleaned_tokens
+
+if __name__ == "__main__":
+    # Tokenize the data
+    positive_tweets = twitter_samples.strings('positive_tweets.json') # 5000 tweets with positive sentiments
+    negative_tweets = twitter_samples.strings('negative_tweets.json') # 5000 tweets with negative sentiments
+    text = twitter_samples.strings('tweets.20150430-223406.json') # 20000 tweets with no sentiments
+    tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+
+    stop_words = stopwords.words('english')
+
+    # Cleaned the sample tweets
+    positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+    negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+
+    positive_cleaned_tokens = []
+    negative_cleaned_tokens = []
+
+    for tokens in positive_tweet_tokens:
+        positive_cleaned_tokens.append(remove_noise(tokens, stop_words))
+    
+    for tokens in negative_tweet_tokens:
+        negative_cleaned_tokens.append(remove_noise(tokens, stop_words))
+
+    
