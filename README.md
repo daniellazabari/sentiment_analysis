532b615f8e9fee55f120a93f4e599720f389bcd8
daniella.zabari <daniella.zabari@post.idc.ac.il>
2022-09-11 14:55:58 +0300

    Prepared data for model

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Sentiment Analysis
 ## What is sentiment analysis?
 Sentiment analysis or sentiment classification fall into the broad catagory of text classification tasks where you are supplied with a phrase, or a list of phrases and your classifier is supposed to tell if the sentiment behind that is positive, negative or neutral (for binary classification problem: we don't take the third attribute). In recent tasks, sentiments like "somewhat positive" and "somewhat negative" are also being considered.
+Here, I used the Naive Bayes classifier of NLTK to perform the modeling of the sentiment analysis.
 
 ## Useful Linkes
 - [nltk](https://www.nltk.org)
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from nltk.corpus import twitter_samples
 from nltk.corpus import stopwords
 from nltk import FreqDist
 import re, string
+import random
 
 def remove_noise(tweet_tokens, stop_words=()):
     '''
@@ -53,6 +54,14 @@ def get_all_words(cleaned_tokens):
         for token in tokens:
             yield token
 
+def get_tweets_for_model(cleaned_tokens):
+    '''
+    A Generator function that converts the tweets from a list of cleaned tokens to dictionaries with 
+    tokens as keys and True as values.
+    '''
+    for tweet_tokens in cleaned_tokens:
+        yield dict([token, True] for token in tweet_tokens)
+
 if __name__ == "__main__":
     # Tokenize the data
     positive_tweets = twitter_samples.strings('positive_tweets.json')
@@ -77,5 +86,18 @@ if __name__ == "__main__":
 
     all_pos_words = get_all_words(positive_cleaned_tokens)
     freq_dist_pos = FreqDist(all_pos_words)
-    print(freq_dist_pos.most_common(10))
+
+    # Prepaaring data for the model
+    positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens)
+    negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens)
     
+    positive_dataset = [(tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model]
+    negative_dataset = [(tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model]
+    dataset = positive_dataset + negative_dataset
+
+    random.shuffle(dataset)
+
+    train_data = dataset[:7000]
+    test_data = dataset[7000:]
+    
+
